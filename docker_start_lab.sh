2cab708e2d6edde38d08cb641a5aed2283dc79a0
arrow2601 <89559887+arrow2601@users.noreply.github.com>
2024-08-09 09:35:53 +0700

    Update docker_start_lab.sh

--- docker_start_lab.sh
+++ docker_start_lab.sh
@@ -3,7 +3,7 @@
 # Fungsi untuk memulai Lab I dengan Docker container dasar
 start_lab_1() {
     echo "Starting Lab"
-    docker run -dit --name dockerlab rehan26/dockerlab
+    docker run -dit --name --privileged dockerlab rehan26/dockerlab
     docker exec -it dockerlab /bin/sh
     echo "Lab I environment started with container name lab1_env"
 }
