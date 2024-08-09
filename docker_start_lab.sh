726b52fe06053ba2ac8d29f9c5f824f9ee833dc9
arrow2601 <89559887+arrow2601@users.noreply.github.com>
2024-08-09 09:53:18 +0700

    Update docker_start_lab.sh

--- docker_start_lab.sh
+++ docker_start_lab.sh
@@ -4,6 +4,7 @@
 start_lab_1() {
     echo "Starting Lab"
     docker run -dit --name dockerlab --privileged rehan26/dockerlab:v3
+    docker exec -it git clone https://github.com/arrow2601/Docker_Lab.git
     docker exec -it dockerlab /bin/sh
     echo "Lab I environment started with container name lab1_env"
 }
