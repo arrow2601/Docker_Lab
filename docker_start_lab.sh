d51d6d564e6cd443014f99d221accf8383b7150e
arrow2601 <89559887+arrow2601@users.noreply.github.com>
2024-08-13 09:04:10 +0700

    Update docker_start_lab.sh

--- docker_start_lab.sh
+++ docker_start_lab.sh
@@ -7,6 +7,8 @@ start_lab_1() {
     docker exec -it dockerlab git clone https://github.com/arrow2601/Docker_Lab.git
     docker exec dockerlab mv Docker_Lab/docker_lab_1.sh /usr/local/bin/docker-lab-1-start
     docker exec dockerlab chmod +x /usr/local/bin/docker-lab-1-start
+    docker exec dockerlab mv Docker_Lab/docker_lab_2.sh /usr/local/bin/docker-lab-2-start
+    docker exec dockerlab chmod +x /usr/local/bin/docker-lab-2-start
     docker exec -it dockerlab /bin/sh
     echo "Lab I environment started with container name lab1_env"
 }
